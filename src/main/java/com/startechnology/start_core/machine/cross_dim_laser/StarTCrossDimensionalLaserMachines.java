e778e4b253949339dc7b0563762896ecaf923dcd
stellaurora <jean1398reborn@gmail.com>
2026-05-25 10:55:04 +1000

    feat: add structure for CDLTA

--- src/main/java/com/startechnology/start_core/machine/cross_dim_laser/StarTCrossDimensionalLaserMachines.java
+++ src/main/java/com/startechnology/start_core/machine/cross_dim_laser/StarTCrossDimensionalLaserMachines.java
@@ -1,6 +1,8 @@
 package com.startechnology.start_core.machine.cross_dim_laser;
 
+import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtceu.api.GTCEuAPI;
+import com.gregtechceu.gtceu.api.GTValues;
 import com.gregtechceu.gtceu.api.data.chemical.ChemicalHelper;
 import com.gregtechceu.gtceu.api.data.tag.TagPrefix;
 import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
@@ -10,6 +12,8 @@ import com.gregtechceu.gtceu.api.machine.multiblock.WorkableElectricMultiblockMa
 import com.gregtechceu.gtceu.api.pattern.FactoryBlockPattern;
 import com.gregtechceu.gtceu.api.pattern.Predicates;
 import com.gregtechceu.gtceu.common.data.GCYMBlocks;
+import com.gregtechceu.gtceu.common.data.GTBlocks;
+import com.gregtechceu.gtceu.common.data.GTMachines;
 import com.gregtechceu.gtceu.common.data.GTRecipeTypes;
 import com.startechnology.start_core.StarTCore;
 import com.startechnology.start_core.machine.StarTMachineUtils;
@@ -20,7 +24,7 @@ public class StarTCrossDimensionalLaserMachines {
 
     public static MultiblockMachineDefinition CROSS_DIMENSIONAL_LASER = StarTCore.START_REGISTRATE
         .multiblock("cross_dimensional_laser", StarTCrossDimensionalLaserMachine::new)
-        .appearanceBlock(() -> StarTMachineUtils.getKjsBlock(("draco_ware_casing")))
+        .appearanceBlock(() -> GTBlocks.ADVANCED_COMPUTER_CASING.get())
         .recipeType(GTRecipeTypes.DUMMY_RECIPES)
         .langValue("Cross-Dimensional Laser Tunneling Array [CDLTA]")
         .tooltips(
@@ -37,37 +41,24 @@ public class StarTCrossDimensionalLaserMachines {
             Component.translatable("block.start_core.breaker_line")
         )
         .pattern(definition -> FactoryBlockPattern.start()
-            .aisle("AAAAAAAAAAAAAAAAA", "A######A#A######A", "A####AA###AA####A", "A###A#######A###A", "A##A#########A##A", "A#A###########A#A", "A#A###########A#A", "AA#############AA", "A###############A", "A###############A", "A###############A", "A###############A", "A###############A", "AA#############AA", "A#A###########A#A", "A#A###########A#A", "A##A#########A##A", "A###A#######A###A", "A####AA###AA####A", "A######A#A######A", "AAAAAAAAAAAAAAAAA") 
-            .aisle("A######A#A######A", "#BBBBBBCDCBBBBBB#", "#BBBBCCCDCCCBBBB#", "#BBBCCCCDCCCCBBB#", "#BBCCCCEEECCCCBB#", "#BCCCEEEEEEECCCB#", "#BCCCEEFFFEECCCB#", "ACCCEEFFDFFEECCCA", "#CCCEEFFFFFEECCC#", "#CCEEFFFDFFFEECC#", "#DDEEFDFDFDFEEDD#", "#CCEEFFFDFFFEECC#", "#CCCEEFFFFFEECCC#", "ACCCEEFFDFFEECCCA", "#BCCCEEFFFEECCCB#", "#BCCCEEEEEEECCCB#", "#BBCCCCEEECCCCBB#", "#BBBCCCCDCCCCBBB#", "#BBBBCCCDCCCBBBB#", "#BBBBBBCDCBBBBBB#", "A######A#A######A") 
-            .aisle("A####AA###AA####A", "#BBBBCCCDCCCBBBB#", "#B#############B#", "#B#############B#", "#B#############B#", "AC#############CA", "AC#############CA", "#C#############C#", "#C#############C#", "#C#############C#", "#D#############D#", "#C#############C#", "#C#############C#", "#C#############C#", "AC#############CA", "AC#############CA", "#B#############B#", "#B#############B#", "#B#############B#", "#BBBBCCCDCCCBBBB#", "A####AA###AA####A") 
-            .aisle("A###A#######A###A", "#BBBCCCCDCCCCBBB#", "#B#############B#", "#B#############B#", "AC#############CA", "#C#############C#", "#C#############C#", "#C#############C#", "#C#############C#", "#E#############E#", "#E#############E#", "#E#############E#", "#C#############C#", "#C#############C#", "#C#############C#", "#C#############C#", "AC#############CA", "#B#############B#", "#B#############B#", "#BBBCCCCDCCCCBBB#", "A###A#######A###A") 
-            .aisle("A##A#########A##A", "#BBCCCCEEECCCCBB#", "#B#############B#", "AC#############CA", "#C#############C#", "#C#############C#", "#C#############C#", "#E#############E#", "#E#############E#", "#E#############E#", "#E#############E#", "#E#############E#", "#E#############E#", "#E#############E#", "#C#############C#", "#C#############C#", "#C#############C#", "AC#############CA", "#B#############B#", "#BBCCCCEEECCCCBB#", "A##A#########A##A") 
-            .aisle("A#A###########A#A", "#BCCCEEEEEEECCCB#", "AC#############CA", "#C#############C#", "#C#############C#", "#E#############E#", "#E#############E#", "#E#############E#", "#E#############E#", "#F#############F#", "#F#############F#", "#F#############F#", "#E#############E#", "#E#############E#", "#E#############E#", "#E#############E#", "#C#############C#", "#C#############C#", "AC#############CA", "#BCCCEEEEEEECCCB#", "A#A###########A#A") 
-            .aisle("A#A###########A#A", "#BCCCEEFFFEECCCB#", "AC#############CA", "#C#############C#", "#C#############C#", "#E#############E#", "#E#############E#", "#F#############F#", "#F#############F#", "#F#############F#", "#D#############D#", "#F#############F#", "#F#############F#", "#F#############F#", "#E#############E#", "#E#############E#", "#C#############C#", "#C#############C#", "AC#############CA", "#BCCCEEFFFEECCCB#", "A#A###########A#A") 
-            .aisle("AA#############AA", "ACCCEEFFFFFEECCCA", "#C#############C#", "#C#############C#", "#E#############E#", "#E#############E#", "#F#############F#", "#F#############F#", "#F#############F#", "#F#############F#", "#F#############F#", "#F#############F#", "#F#############F#", "#F#############F#", "#F#############F#", "#E#############E#", "#E#############E#", "#C#############C#", "#C#############C#", "ACCCEEFFFFFEECCCA", "AA#############AA") 
-            .aisle("A###############A", "#DDDEEFFDFFEEDDD#", "#D#############D#", "#D#############D#", "#E#############E#", "#E#############E#", "#F#############F#", "#D#############D#", "#F#############F#", "#D#############D#", "#D#############D#", "#D#############D#", "#F#############F#", "#D#############D#", "#F#############F#", "#E#############E#", "#E#############E#", "#D#############D#", "#D#############D#", "#DDDEEFFDFFEEDDD#", "A###############A") 
-            .aisle("AA#############AA", "ACCCEEFFFFFEECCCA", "#C#############C#", "#C#############C#", "#E#############E#", "#E#############E#", "#F#############F#", "#F#############F#", "#F#############F#", "#F#############F#", "#F#############F#", "#F#############F#", "#F#############F#", "#F#############F#", "#F#############F#", "#E#############E#", "#E#############E#", "#C#############C#", "#C#############C#", "ACCCEEFFFFFEECCCA", "AA#############AA") 
-            .aisle("A#A###########A#A", "#BCCCEEFFFEECCCB#", "AC#############CA", "#C#############C#", "#C#############C#", "#E#############E#", "#E#############E#", "#F#############F#", "#F#############F#", "#F#############F#", "#D#############D#", "#F#############F#", "#F#############F#", "#F#############F#", "#E#############E#", "#E#############E#", "#C#############C#", "#C#############C#", "AC#############CA", "#BCCCEEFFFEECCCB#", "A#A###########A#A") 
-            .aisle("A#A###########A#A", "#BCCCEEEEEEECCCB#", "AC#############CA", "#C#############C#", "#C#############C#", "#E#############E#", "#E#############E#", "#E#############E#", "#E#############E#", "#F#############F#", "#F#############F#", "#F#############F#", "#E#############E#", "#E#############E#", "#E#############E#", "#E#############E#", "#C#############C#", "#C#############C#", "AC#############CA", "#BCCCEEEEEEECCCB#", "A#A###########A#A") 
-            .aisle("A##A#########A##A", "#BBCCCCEEECCCCBB#", "#B#############B#", "AC#############CA", "#C#############C#", "#C#############C#", "#C#############C#", "#E#############E#", "#E#############E#", "#E#############E#", "#E#############E#", "#E#############E#", "#E#############E#", "#E#############E#", "#C#############C#", "#C#############C#", "#C#############C#", "AC#############CA", "#B#############B#", "#BBCCCCEEECCCCBB#", "A##A#########A##A") 
-            .aisle("A###A#######A###A", "#BBBCCCCDCCCCBBB#", "#B#############B#", "#B#############B#", "AC#############CA", "#C#############C#", "#C#############C#", "#C#############C#", "#C#############C#", "#E#############E#", "#E#############E#", "#E#############E#", "#C#############C#", "#C#############C#", "#C#############C#", "#C#############C#", "AC#############CA", "#B#############B#", "#B#############B#", "#BBBCCCCDCCCCBBB#", "A###A#######A###A") 
-            .aisle("A####AA###AA####A", "#BBBBCCCDCCCBBBB#", "#B#############B#", "#B#############B#", "#B#############B#", "AC#############CA", "AC#############CA", "#C#############C#", "#C#############C#", "#C#############C#", "#D#############D#", "#C#############C#", "#C#############C#", "#C#############C#", "AC#############CA", "AC#############CA", "#B#############B#", "#B#############B#", "#B#############B#", "#BBBBCCCDCCCBBBB#", "A####AA###AA####A") 
-            .aisle("A######A#A######A", "#BBBBBBCDCBBBBBB#", "#BBBBCCCDCCCBBBB#", "#BBBCCCCDCCCCBBB#", "#BBCCCCEEECCCCBB#", "#BCCCEEEEEEECCCB#", "#BCCCEEFFFEECCCB#", "ACCCEEFFDFFEECCCA", "#CCCEEFFFFFEECCC#", "#CCEEFFFDFFFEECC#", "#DDEEFDF@FDFEEDD#", "#CCEEFFFDFFFEECC#", "#CCCEEFFFFFEECCC#", "ACCCEEFFDFFEECCCA", "#BCCCEEFFFEECCCB#", "#BCCCEEEEEEECCCB#", "#BBCCCCEEECCCCBB#", "#BBBCCCCDCCCCBBB#", "#BBBBCCCDCCCBBBB#", "#BBBBBBCDCBBBBBB#", "A######A#A######A") 
-            .aisle("AAAAAAAAAAAAAAAAA", "A######A#A######A", "A####AA###AA####A", "A###A#######A###A", "A##A#########A##A", "A#A###########A#A", "A#A###########A#A", "AA#############AA", "A###############A", "A###############A", "A###############A", "A###############A", "A###############A", "AA#############AA", "A#A###########A#A", "A#A###########A#A", "A##A#########A##A", "A###A#######A###A", "A####AA###AA####A", "A######A#A######A", "AAAAAAAAAAAAAAAAA") 
-            .where("A",  Predicates.blocks(ChemicalHelper.getBlock(TagPrefix.frameGt, GTCEuAPI.materialManager.getMaterial("abyssal_alloy"))))
+            .aisle("#AA#BBB#AA#", "AAA#BBB#AAA", "AAA#BBB#AAA", "#####C#####", "BBB#CCC#BBB", "BBBCCCCCBBB", "BBB#CCC#BBB", "#####C#####", "AAA#BBB#AAA", "AAA#BBB#AAA", "#AA#BBB#AA#") 
+            .aisle("AAA#BBB#AAA", "ADDDDDDDDDA", "ADA#BDB#ADA", "#D##CDC##D#", "BDBCCDCCBDB", "BDDDDEDDDDB", "BDBCCDCCBDB", "#D##CDC##D#", "ADA#BDB#ADA", "ADDDDDDDADA", "AAA#DBD#AAA") 
+            .aisle("#AA#BBB#AA#", "AAA#BFB#AAA", "AAA#BBB#AAA", "#####C#####", "BBB#CCC#BBB", "BBBCC#CCBBB", "BBB#CCC#BBB", "#####C#####", "AAA#BDB#AAA", "AAA#DGD#AAA", "#AA#DBD#AA#") 
             .where("#", Predicates.any())
-            .where("B", Predicates.blocks(StarTMachineUtils.getKjsBlock("draneko_casing")))
-            .where("C", Predicates.blocks(GCYMBlocks.CASING_ATOMIC.get()))
-            .where("D", Predicates.blocks(StarTMachineUtils.getKjsBlock("draco_ware_casing"))
-                .or(Predicates.abilities(PartAbility.IMPORT_FLUIDS).setMaxGlobalLimited(2))
-                .or(Predicates.abilities(PartAbility.INPUT_ENERGY).setMaxGlobalLimited(2))
-                .or(Predicates.abilities(PartAbility.MAINTENANCE).setExactLimit(1)))
-            .where("E", Predicates.blocks(StarTMachineUtils.getKjsBlock("abyssal_inductor_hull")))
-            .where("F", Predicates.blocks(StarTMachineUtils.getKjsBlock("draco_resilient_fusion_glass")))
+            .where("A", Predicates.blocks(GTBlocks.HIGH_POWER_CASING.get()))
+            .where("B", Predicates.blocks(GTBlocks.COMPUTER_CASING.get()))
+            .where("C", Predicates.blocks(StarTMachineUtils.getKjsBlock("reinforced_fusion_glass")))
+            .where("D", Predicates.blocks(GTBlocks.ADVANCED_COMPUTER_CASING.get()))
+            .where("E", Predicates.abilities(PartAbility.INPUT_LASER)
+                .or(Predicates.abilities(PartAbility.OUTPUT_LASER))
+                .setExactLimit(1))
             .where("@", Predicates.controller(Predicates.blocks(definition.get())))
+            .where("G", Predicates.blocks(GTMachines.ITEM_IMPORT_BUS[GTValues.ULV].getBlock()))
             .build()
-        ).workableCasingModel(KubeJS.id("block/casings/end_multis/draco_ware_casing"),
-            StarTCore.resourceLocation("block/overlay/abyssal_containment"))
+        ).workableCasingModel(
+            GTCEu.id("block/casings/hpca/computer_casing"),
+            GTCEu.id("block/multiblock/hpca")
+        )
         .register();
 
         public static final void init() {};
