19020254e7013b3fd163bcfefed6cc03f3b17236
Spear of Heaven <rishabh.chaudhuri89671@gmail.com>
2026-06-03 18:37:05 +0530

    Started working on the Laser Mirror
     On branch Laser-Mirror
     Changes to be committed:
    	new file:   src/main/java/com/startechnology/pipelike/laser/LaserMirrorProperties.java
    	new file:   src/main/java/com/startechnology/pipelike/laser/LaserMirrorType.java
    	new file:   src/main/java/com/startechnology/start_core/block/LaserMirror.java

---
+++ src/main/java/com/startechnology/pipelike/laser/LaserMirrorProperties.java
@@ -0,0 +1,9 @@
+package com.gregtechceu.gtceu.common.pipelike.laser;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class LaserMirrorProperties {
+
+    public static final LaserMirrorProperties INSTANCE = new LaserMirrorProperties();
+}
